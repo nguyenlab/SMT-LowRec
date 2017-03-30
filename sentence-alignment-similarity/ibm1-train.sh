619414db6a6af702b393f8481dae8ca657f549a2
NguyenLab <nguyenlab.jaist@gmail.com>
2017-03-31 00:39:23 +0900

    Add files via upload

--- sentence-alignment-similarity/ibm1-train.sh
+++ sentence-alignment-similarity/ibm1-train.sh
@@ -2,18 +2,16 @@
 
 JAVA_DIR=sentence_alignment
 
-SRCLANG=ja
-TRGLANG=vi
-
 
 SETUP=ibm1
 
-
 IBM1_DIR=ibm1
-SRC_MERGE_LENGTH=$IBM1_DIR/length-based.$SRCLANG
-TRG_MERGE_LENGTH=$IBM1_DIR/length-based.$TRGLANG
+
+cat data/bilingual/ja-vi.ja length-based.ja > ibm1/data.ja
+cat data/bilingual/ja-vi.vi length-based.vi > ibm1/data.vi
+
 LOOP=5
 IBM1_FILE=ibm1.model
 
 java -cp $JAVA_DIR/target/classes:$JAVA_DIR/target/lib/* Sentence_Aligner $SETUP \
-	$SRC_MERGE_LENGTH $TRG_MERGE_LENGTH $LOOP $IBM1_DIR $IBM1_FILE
+	ibm1/data.ja ibm1/data.vi $LOOP $IBM1_DIR $IBM1_FILE
